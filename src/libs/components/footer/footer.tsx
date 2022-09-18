1ee03d9865a1e3d56b6cf6390cd2a1ffe18aaa6e
Barhon Moshe Dasa <moshebdasa@gmail.com>
2022-09-18 20:21:37 +0300

    V1

--- src/libs/components/footer/footer.tsx
+++ src/libs/components/footer/footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer style={{ position: "fixed", bottom: 0 }}>
       <div>
-        <p>&copy; 2022 Barhon Moshe Dasa</p>
+        <p>&copy; 2022 Barhon Moshe Dasa - V1</p>
       </div>
     </footer>
   );
