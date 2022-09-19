3bb3cb2f40381126360ffdad612e0b667b4b62e1
Barhon Moshe Dasa <moshebdasa@gmail.com>
2022-09-19 10:42:51 +0300

    V9

--- src/libs/components/footer/footer.tsx
+++ src/libs/components/footer/footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer style={{ position: "fixed", bottom: 0 }}>
       <div>
-        <p>&copy; 2022 Barhon Moshe Dasa - V7</p>
+        <p>&copy; 2022 Barhon Moshe Dasa - V9</p>
       </div>
     </footer>
   );
--- src/routes/general/scoreboard/scoreboard.tsx
+++ src/routes/general/scoreboard/scoreboard.tsx
@@ -4,40 +4,45 @@ import GameResult from "./components/game-result";
 function Scoreboard() {
   return (
     <>
-      <h1>בית א</h1>
-      <GameResult
-        gameStatus={GameStatusType.notStarted}
-        pointsTeamA={2}
-        pointsTeamB={0}
-        teamAId={1}
-        teamBId={2}
-      ></GameResult>
-
-      <GameResult
-        gameStatus={GameStatusType.notStarted}
-        pointsTeamA={4}
-        pointsTeamB={1}
-        teamAId={4}
-        teamBId={3}
-      ></GameResult>
-
-      <h1>בית ב</h1>
-      <GameResult
-        gameStatus={GameStatusType.notStarted}
-        pointsTeamA={2}
-        pointsTeamB={0}
-        teamAId={1}
-        teamBId={2}
-      ></GameResult>
-
-      <GameResult
-        gameStatus={GameStatusType.notStarted}
-        pointsTeamA={4}
-        pointsTeamB={1}
-        teamAId={4}
-        teamBId={3}
-      ></GameResult>
+      <h4> Scoreboard</h4>
+      <h1> בקרוב....</h1>
     </>
+
+    // <>
+    //   <h1>בית א</h1>
+    //   <GameResult
+    //     gameStatus={GameStatusType.notStarted}
+    //     pointsTeamA={2}
+    //     pointsTeamB={0}
+    //     teamAId={1}
+    //     teamBId={2}
+    //   ></GameResult>
+
+    //   <GameResult
+    //     gameStatus={GameStatusType.notStarted}
+    //     pointsTeamA={4}
+    //     pointsTeamB={1}
+    //     teamAId={4}
+    //     teamBId={3}
+    //   ></GameResult>
+
+    //   <h1>בית ב</h1>
+    //   <GameResult
+    //     gameStatus={GameStatusType.notStarted}
+    //     pointsTeamA={2}
+    //     pointsTeamB={0}
+    //     teamAId={1}
+    //     teamBId={2}
+    //   ></GameResult>
+
+    //   <GameResult
+    //     gameStatus={GameStatusType.notStarted}
+    //     pointsTeamA={4}
+    //     pointsTeamB={1}
+    //     teamAId={4}
+    //     teamBId={3}
+    //   ></GameResult>
+    // </>
   );
 }
 
